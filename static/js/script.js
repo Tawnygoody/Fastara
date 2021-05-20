94399997975ed93b9b6487cc495b135a0b251338
Tawnygoody <chrisgoodfellow1@hotmail.com>
2021-05-20 11:09:36 +0000

    autoplay function added to carousel

--- static/js/script.js
+++ static/js/script.js
@@ -4,5 +4,10 @@ $(document).ready(function(){
     $('.carousel.carousel-slider').carousel({
         fullWidth: true,
         indicators: true
-    });
+    }, setTimeout(autoplay, 4000));
   });
+
+  function autoplay() { // Help with autoplay for carousel taken from https://stackoverflow.com/questions/36581504/materialize-carousel-slider-autoplay
+      $('.carousel').carousel('next');
+      setTimeout(autoplay, 5000);
+  }
