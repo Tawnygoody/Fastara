d24cc2634bf026ffedd06b4d2c69872e943111ca
Tawnygoody <chrisgoodfellow1@hotmail.com>
2021-05-20 10:30:55 +0000

    carousel added to home.html

--- app.py
+++ app.py
@@ -59,7 +59,7 @@ def login():
         if existing_user:
             # Ensure hashed password matches user input
             if check_password_hash(
-                existing_user["password"], request.form.get("password")):
+            existing_user["password"], request.form.get("password")):
                     session["user"] = request.form.get("username").lower()
                     flash("Welcome, {}".format(request.form.get("username")))
                     return redirect(url_for(
--- static/js/script.js
+++ static/js/script.js
@@ -1,4 +1,8 @@
 $(document).ready(function(){
     $(".sidenav").sidenav({edge:"right"});
     $('.modal').modal();
+    $('.carousel.carousel-slider').carousel({
+        fullWidth: true,
+        indicators: true
+    });
   });
